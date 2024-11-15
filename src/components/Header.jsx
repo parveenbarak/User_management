0791aa65b4beaa533f32ba3e09da6b49d9270d35
parveen barak <parveenbarak117@gmail.com>
2024-11-15 21:31:37 +0530

    Header Created

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-blue-600 text-white py-4 text-center">
+      <h1 className="text-xl font-bold">User Management System</h1>
+    </header>
+  );
+}
+
+export default Header;
